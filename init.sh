9bcaf3095a07b13d76d4b090d09b54cde5ac35b5
Thanayut Seethongchuen <thanayut.see@outlook.com>
2021-07-22 15:19:30 +0700

    move ansible to this repo

---
+++ init.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -euo pipefail
+
+USERNAME=$(who | awk '{print $1}')
+>2 echo "sudo no password with \"$USERNAME\""
+
+if [[ $EUID -ne 0 ]]; then
+   >2 echo "This script must be run as root"
+   exit 1
+fi
+
+dnf install -y emacs
+cat > /etc/sudoers.d/${USERNAME} \
+    <<<"""${USERNAME}        ALL=(ALL)       NOPASSWD: ALL"""
+sudo -H -u ${USERNAME} python3 -m pip install "ansible<3"
---
+++ run.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+ansible-playbook -vv -c local playbook.yml
