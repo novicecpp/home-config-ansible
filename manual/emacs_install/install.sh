0775e0d103c63c83f41e0d74056f9ef487ae0420
Thanayut Seethongchuen <thanayut.see@outlook.com>
2021-08-21 07:45:03 +0700

    change install commit to pgtk branch (for support hidpi)

--- manual/emacs_install/install.sh
+++ manual/emacs_install/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -euo pipefail
-VERSION_CHECKOUT=45cdc89986
+VERSION_CHECKOUT=4ed859359c
 rm -rf emacs
 git clone git://git.sv.gnu.org/emacs.git
 cd emacs
 git checkout $VERSION_CHECKOUT
 ./autogen.sh
-./configure --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64 --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --with-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation
+./configure --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64 --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --with-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation --with-pgtk
 make -j $(cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l)
-sudo make install
+sudo make install install-etc #install-etc for emacs icon
+cd ..
 
 # copy emacs.service to systemd
 USER_SYSTEMD_PATH=~/.config/systemd/user/
