db71ee5e037022a7fc9a39b6cf4bb27f5396b0b1
Thanayut Seethongchuen <thanayut.see@outlook.com>
2022-01-10 08:19:29 +0100

    fix remake (remove --with-pgtk)

--- manual/emacs_install/remake.sh
+++ manual/emacs_install/remake.sh
@@ -3,8 +3,9 @@ set -euo pipefail
 cd emacs
 git clean -fdx
 ./autogen.sh
-./configure --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64 --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --without-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation --with-pgtk
+./configure --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64 --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --without-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation
 make NATIVE_FULL_AOT=1 -j $(cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l)
 systemctl stop --user emacs
 sudo make uninstall
 sudo make install
+systemctl start --user emacs
