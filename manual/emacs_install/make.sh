17156c854cdbf9892d047516d72263a51b103716
Thanayut Seethongchuen <thanayut.see@outlook.com>
2023-03-21 23:19:20 +0100

    update emacs ./configure options
    -ljemalloc does not work

--- manual/emacs_install/make.sh
+++ manual/emacs_install/make.sh
@@ -10,9 +10,9 @@ git clean -fdx
 ./autogen.sh
 ./configure \
     --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64 --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info \
-    --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --without-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation --with-tree-sitter \
-    CFLAGS='-O3 -march=native -mtune=native -flto=auto -ffat-lto-objects -pipe -Wall' \
-    LDFLAGS='-ljemalloc -lm -pthread'
+    --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --without-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation --with-tree-sitter --with-pgtk --with-xwidgets \
+    --enable-link-time-optimization --enable-autodepend \
+    CFLAGS="-O2 -march=native -pipe -Wall"
 
 make -j $(( $(nproc) - 1))
 systemctl stop --user emacs
