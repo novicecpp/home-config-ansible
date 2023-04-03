44f5152056307bbed286a21628001f7cd63f7a50
Thanayut Seethongchuen <thanayut.see@outlook.com>
2023-04-04 01:47:18 +0200

    update cleanup_cache.sh, update emacs new head

--- manual/emacs_install/cleanup_cache.sh
+++ manual/emacs_install/cleanup_cache.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 set -x
 systemctl stop --user emacs
-rm -rf ~/.emacs.d/eln-cache/ ~/.emacs.d/elpa
+pushd ~/.emacs.d/
+rm -rf eln-cache elpa straight tree-sitter
+popd
 # FIXME: emacs still does not exit after installing package
 #emacs --eval '(progn (sit-for 2) (sleep-for 2) (save-buffers-kill-terminal))'
 #systemctl start --user emacs
+emacs
