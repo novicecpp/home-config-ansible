c5b05c674f50b4fe2e5aef4dd7383111e90e4d14
Thanayut Seethongchuen <thanayut.see@outlook.com>
2023-04-28 01:03:32 +0200

    bump emacs version

--- manual/emacs_install/make.sh
+++ manual/emacs_install/make.sh
@@ -11,7 +11,8 @@ git clean -fdx
 ./autogen.sh
 ./configure \
     --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64 --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info \
-    --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --without-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation --with-tree-sitter --with-pgtk --with-xwidgets \
+    --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --without-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation --with-tree-sitter --with-xwidgets \
+    --with-pgtk \
     --enable-link-time-optimization --enable-autodepend \
     CFLAGS="-O2 -march=native -pipe -Wall"
 
