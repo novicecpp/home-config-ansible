78f8cbf03618a878e68f07b5dd74eca34bf13017
Thanayut Seethongchuen <thanayut.see@outlook.com>
2023-05-22 06:24:14 +0200

    echo

--- manual/backup/backup.sh
+++ manual/backup/backup.sh
@@ -12,6 +12,9 @@ MACHINE_NAME=${MACHINE_NAME:-x1g2}
 DIR_NAME=${DIR_NAME:-myhome}
 printf -v BACKUP_PATH '%s/%(%Y-%m-%d)T_%s_%s.tar.zstd' "$DSTDIR" -1 "$MACHINE_NAME" "$DIR_NAME"
 
+echo "Backup from $SRCDIR to $BACKUP_PATH"
 
-tar --ignore-failed-read -cf - "${SRCDIR}" | pv -brt | zstd -T0 - -o "${BACKUP_PATH}"
+tar --ignore-failed-read -cf - "${SRCDIR}" | pv -brt | zstd -T0 -o "${BACKUP_PATH}" -
+
+echo "Running checksum..."
 sha256sum "${BACKUP_PATH}" > "${BACKUP_PATH}.sha256"
