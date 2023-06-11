0ad292522a1427e1bdfc0e6ae64b8cad23677448
Thanayut Seethongchuen <thanayut.see@outlook.com>
2023-06-11 19:52:45 +0200

    [backup.sh] safety net

--- manual/backup/backup.sh
+++ manual/backup/backup.sh
@@ -12,6 +12,11 @@ MACHINE_NAME=${MACHINE_NAME:-x1g2}
 DIR_NAME=${DIR_NAME:-myhome}
 printf -v BACKUP_PATH '%s/%(%Y-%m-%d)T_%s_%s.tar.zstd' "$DSTDIR" -1 "$MACHINE_NAME" "$DIR_NAME"
 
+if [[ ! -d "${BACKUP_PATH}" ]]; then
+    >&2 echo "Error: backup path \"${BACKUP_PATH}\" is not directory."
+    exit 1
+fi
+
 echo "Backup from $SRCDIR to $BACKUP_PATH"
 
 tar --ignore-failed-read -cf - "${SRCDIR}" | pv -brt | zstd -T0 - > "${BACKUP_PATH}"
