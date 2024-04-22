5fde5087d41697c266c5305611675594d6c20936
Thanayut Seethongchuen <thanayut.see@outlook.com>
2024-04-22 04:03:27 +0200

    update emacs build flag

--- manual/emacs_install/make.sh
+++ manual/emacs_install/make.sh
@@ -8,14 +8,22 @@ fi
 
 cd emacs
 git clean -fdx
-./autogen.sh
+./autogen.sh autoconf
+# copy from fedora flag
+# --without-gpm
+# replaced/enabled by pgtk
+#--with-x-toolkit=gtk3 --with-xft --with-xpm --with-cairo
+# deprecated
+# --with-json
 ./configure \
     --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64 --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info \
-    --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-x-toolkit=gtk3 --with-gpm=no --without-xwidgets --with-modules --with-harfbuzz --with-cairo --with-json --with-mailutils --with-native-compilation --with-tree-sitter --with-xwidgets \
-    --with-pgtk \
-    --enable-link-time-optimization --enable-autodepend \
-    CFLAGS="-O2 -march=native -pipe -Wall"
-
+    --without-gpm \
+    --with-dbus --with-harfbuzz --with-xwidgets --with-mailutils --with-sqlite3 --with-libsystemd \
+    --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-webp \
+    --with-native-compilation --with-tree-sitter --with-pgtk \
+    CFLAGS="-O3 -m64 -march=native -mtune=native -pipe -Wall -Wp,-U_FORTIFY_SOURCE,-D_FORTIFY_SOURCE=3 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong" \
+    CXXFLAGS="-O3 -m64 -march=native -mtune=native -pipe -Wall -Wp,-U_FORTIFY_SOURCE,-D_FORTIFY_SOURCE=3 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong" \
+    LDFLAGS="-Wl,-z,relro"
 make -j $(( $(nproc) - 1))
 systemctl stop --user emacs
 sudo make uninstall
