24fd41b78d9fd4113a393d4b52ed59789e93cb52
Thanayut Seethongchuen <thanayut.see@outlook.com>
2024-05-14 09:19:18 +0200

    use pipenv
    Ansible always break when pacman's python upgrade to newer version
    while current ansible process still running on the older version

--- example_run.sh
+++ example_run.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 # symlink playbook to current directory
-ln -s private/playbook/x1g2.yml x1g2.yml
-ansible-playbook -vvv -i private/inventory/x1g2/hosts.yml x1g2.yml
+ansible-playbook -vvv -i private/inventory/x1g2/hosts.yml private/playbook/x1g2.yml
