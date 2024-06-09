b456cc4871078e43eb90dfd538d7992dffd327da
Thanayut Seethongchuen <thanayut.see@outlook.com>
2024-06-09 12:16:45 +0200

    build emacs in makepkg

---
+++ manual/emacs_install/make_aur.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+set -euo pipefail
+set -x
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+workdir=$SCRIPT_DIR/aur
+mkdir -p $workdir
+rm -rf $workdir/*
+#commit_hash=$(git ls-remote https://github.com/emacs-mirror/emacs HEAD | awk '{print $1}')
+#commit_hash=$(cat $SCRIPT_DIR/emacs_master)
+#sed -E -e "s/(^pkgver=)([0-9a-f]+\.git)/\1${commit_hash:0:8}\.git/g" -e "s/(emacs#commit=)([0-9a-f]+)/\1${commit_hash}/g" PKGBUILD > $workdir/PKGBUILD
+cp PKGBUILD aur
+pushd $workdir
+makepkg -f
+sudo pacman -U emacs-master*.pkg.tar.zst --noconfirm
+popd
--- manual/emacs_install/update_all.sh
+++ manual/emacs_install/update_all.sh
@@ -2,8 +2,14 @@
 set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR
+#bash clone.sh checkout
 bash clone.sh master
-bash make.sh
+source /etc/os-release
+if [[ $ID = 'arch' ]]; then
+    bash make_aur.sh
+else
+    bash make.sh
+fi
 emacs --eval '(progn (straight-pull-all) (save-buffers-kill-terminal))'
 systemctl restart --user emacs --no-block
 popd
