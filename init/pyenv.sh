b489d7d5f2e5cafe4ec463a7c035c4523b0c19b8
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-03-06 23:05:18 +0100

    fix init pyenv

--- init/pyenv.sh
+++ init/pyenv.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 PYENV_VERSION=2.5.0
-PYENV_ROOT=.venv
+PYENV_ROOT=.pyenv
 
 # https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
 
@@ -15,30 +15,33 @@ trim() {
     printf '%s' "$var"
 }
 
-# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pushd $SCRIPT_DIR
+# create it at top level dir
+ROOT_DIR=$(git rev-parse --show-toplevel)
+pushd "${ROOT_DIR}"
 
 # install pyenv
+# skip if it already cloned
 if [[ ! -d $PYENV_ROOT/.git ]]; then
     git clone --branch "v${PYENV_VERSION}" https://github.com/pyenv/pyenv.git $PYENV_ROOT
 fi
-
+# int pyenv env
 export PYENV_ROOT
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init - bash)"
 
 # getting python version from Pipfile
-PYTHON_VERSION=$(trim $(grep python_version Pipfile | awk -F '=' '{print $2}'))
+PYTHON_VERSION=$(trim "$(grep python_version Pipfile | awk -F '=' '{print $2}')")
+
 # install python
-pyenv install -s $PYTHON_VERSION
-pyenv local $PYTHON_VERSION
+pyenv install -s "${PYTHON_VERSION}"
+pyenv local "${PYTHON_VERSION}"
 pyenv rehash
 
-# install
+# install venv
 python -m venv .venv --prompt "home-config-ansible"
 source .venv/bin/activate
 pip install -U pip
 pip install pipenv
 pipenv install
+
 popd
