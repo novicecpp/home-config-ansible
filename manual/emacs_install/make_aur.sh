0dcfbb93c0af2224a5ad8d4a6cd8256e80288f31
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-03-11 22:27:01 +0100

    tidy aur build
    Also change from -O3 to -O2 hope it will crash-less

--- manual/emacs_install/make_aur.sh
+++ manual/emacs_install/make_aur.sh
@@ -10,7 +10,7 @@ rm -rf $workdir/*
 #commit_hash=$(git ls-remote https://github.com/emacs-mirror/emacs HEAD | awk '{print $1}')
 #commit_hash=$(cat $SCRIPT_DIR/emacs_master)
 #sed -E -e "s/(^pkgver=)([0-9a-f]+\.git)/\1${commit_hash:0:8}\.git/g" -e "s/(emacs#commit=)([0-9a-f]+)/\1${commit_hash}/g" PKGBUILD > $workdir/PKGBUILD
-cp PKGBUILD aur
+cp -r PKGBUILD emacs emacs_master $workdir
 pushd $workdir
 makepkg -f
 sudo pacman -U emacs-master*.pkg.tar.zst --noconfirm
