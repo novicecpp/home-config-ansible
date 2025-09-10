64460976d02da40714c186a013927b65a45aa28b
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-09-10 11:25:04 +0200

    commit emacs_master before checkout latest master

--- manual/emacs_install/clone.sh
+++ manual/emacs_install/clone.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-EMACS_HEAD_FILE=${EMACS_HEAD_FILE:-./emacs_master}
+set -euo pipefail
+
+EMACS_HEAD_FILE=$(realpath ${EMACS_HEAD_FILE:-./emacs_master})
 
 if ! [[ $# == 1 && $1 =~ ^(checkout|-c|master|-m)$ ]]; then
     >&2 echo "Usage: "
@@ -13,18 +15,27 @@ if [[ ! -d "emacs" ]]; then
     #git clone git://git.sv.gnu.org/emacs.git emacs
 fi
 
-pushd emacs
-git fetch origin master
-
 if [[ $1 == 'checkout' || $1 == '-c' ]]; then
-    git checkout $(cat ../emacs_master)
-
+    git checkout $(cat $EMACS_HEAD_FILE)
 fi
 
+
 if [[ $1 == 'master' || $1 == '-m' ]]; then
+    FILE_TO_CHECK="${EMACS_HEAD_FILE}"
+    if [[ -n $(git status --porcelain -- "${FILE_TO_CHECK}") ]]; then
+        git stash
+        git checkout stash@{0} -- "${FILE_TO_CHECK}"
+        EMACS_COMMIT_HASH="$(cat ${FILE_TO_CHECK})"
+        COMMIT_MESSAGE="Auto bump emacs head to ${EMACS_COMMIT_HASH}"
+        echo "Commiting emacs_master with emacs commit: ${EMACS_COMMIT_HASH}"
+        git add "${FILE_TO_CHECK}"
+        git commit -m "${COMMIT_MESSAGE}"
+        git stash apply
+    fi
+    pushd emacs
     git fetch origin
     EMACS_HEAD=$(git rev-parse origin/master)
     git checkout $EMACS_HEAD
-    echo $EMACS_HEAD > ../$EMACS_HEAD_FILE
+    echo -n $EMACS_HEAD > $EMACS_HEAD_FILE
+    popd
 fi
-popd
