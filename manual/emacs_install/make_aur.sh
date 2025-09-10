58444aed9911ae59848011e8be0dedb48f394efa
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-09-10 11:24:45 +0200

    proper pkgbuild

--- manual/emacs_install/make_aur.sh
+++ manual/emacs_install/make_aur.sh
@@ -5,12 +5,10 @@ set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 workdir=$SCRIPT_DIR/aur
-mkdir -p $workdir
-rm -rf $workdir/*
-#commit_hash=$(git ls-remote https://github.com/emacs-mirror/emacs HEAD | awk '{print $1}')
-#commit_hash=$(cat $SCRIPT_DIR/emacs_master)
-#sed -E -e "s/(^pkgver=)([0-9a-f]+\.git)/\1${commit_hash:0:8}\.git/g" -e "s/(emacs#commit=)([0-9a-f]+)/\1${commit_hash}/g" PKGBUILD > $workdir/PKGBUILD
-cp -r PKGBUILD emacs emacs_master $workdir
+mkdir -p ${workdir}
+rm -rf ${workdir}/*
+cp -r PKGBUILD emacs_master $workdir
+tar cf ${workdir}/emacs.tar emacs
 pushd $workdir
 makepkg -f
 sudo pacman -U emacs-master*.pkg.tar.zst --noconfirm
